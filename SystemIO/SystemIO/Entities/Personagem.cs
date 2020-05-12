5df5b0467e448a45ae092c4d1c5a8127e64ffb66
Edson <edsont8@hotmail.com>
2020-05-12 02:45:26 -0300

    Funcional.

---
+++ SystemIO/SystemIO/Entities/Personagem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemIO.Entities
+{
+    public class Personagem
+    {
+        public string Nome { get; set; }
+        public string DataDeNascimento { get; set; }
+        public Personagem(string nome, string dataDeNascimento)
+        {
+            Nome = nome;
+            DataDeNascimento = dataDeNascimento;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome + "\n" + "Data de Nascimento: " + DataDeNascimento + "\n";
+        }
+    }
+}
--- SystemIO/SystemIO/Program.cs
+++ SystemIO/SystemIO/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+using SystemIO.Entities;
 
 namespace SystemIO
 {
@@ -11,6 +10,49 @@ namespace SystemIO
     {
         static void Main(string[] args)
         {
+            List<Personagem> personagens = new List<Personagem>();
+
+            try
+            {
+                string diretorio = @"C:\Senai\Personagens";
+                string caminhoArquivoDestino = @"C:\Senai\Personagens\Personagens.txt";
+                string[] listaDeArquivos = Directory.GetFiles(diretorio);
+
+                if (listaDeArquivos.Length > 0)
+                {
+                    FileStream arquivoDestino = File.Open(caminhoArquivoDestino, FileMode.OpenOrCreate);
+                    arquivoDestino.Close();
+
+                    List<string> linhasDestino = new List<string>();
+                    using (StreamWriter sw = File.AppendText(caminhoArquivoDestino))
+                    {
+                        foreach (string caminhoArquivo in listaDeArquivos)
+                        {
+                            var linhas = (File.ReadLines(caminhoArquivo));
+
+                            string[] props = linhas.ToArray();
+                            string nome = props[0];
+                            string dataDeNascimento = props[1];
+
+                            Personagem personagem = new Personagem(nome, dataDeNascimento);
+                            personagens.Add(personagem);
+                        }
+                        var persona = personagens.OrderBy(p => p.Nome);
+
+                        foreach (var p in persona)
+                        {
+                            Console.WriteLine(p);
+                            sw.WriteLine(p);
+                        }
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro!!!");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
